1a5a6787c315a48a32b803efd2beeeec4d9520c7
chyzwar <schizek.marcin@gmail.com>
2025-08-04 11:51:38 +0200

    chore(): setup tooling and rpeository

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineProject } from "vitest/config";
+
+const projects = [
+
+];
+
+export default defineProject({
+  test: {
+    projects: projects.map((name) => {
+      return {
+        extends: `./packages/${name}/vitest.config.ts`,
+        test: {
+          root: `./packages/${name}/`,
+          name,
+          include: [
+            "src/**/*.test.ts",
+            "src/**/*.test.tsx",
+          ],
+        },
+      };
+    }),
+  },
+});
