8bacb8b91fbd39be851c53912bb048b6ce480758
chyzwar <schizek.marcin@gmail.com>
2025-08-08 21:30:53 +0200

    chore(): benchmark tweaks

--- packages/sprites-benchmarks/src/__bench__/generate.bench.ts
+++ packages/sprites-benchmarks/src/__bench__/generate.bench.ts
@@ -1,19 +1,61 @@
 import { globSync, readFileSync } from "node:fs";
 import { basename, join, resolve } from "node:path";
+import { describe } from "node:test";
+import {
+  type DataLayout,
+  type ImgLayout,
+  generateImage as mapboxGenerateImage,
+  generateLayout as mapboxGenerateLayout,
+} from "@mapbox/spritezero";
+import {
+  generateImage as mapkaGenerateImage,
+  generateLayout as mapkaGenerateLayout,
+} from "@mapka/sprites-generate";
+
 import { bench } from "vitest";
 
-console.log(resolve(join(import.meta.dirname, "../../../sprites-generate/src/__tests__/fixture/svg/*.svg")));
+console.log(
+  resolve(join(import.meta.dirname, "../../../sprites-generate/src/__tests__/fixture/svg/*.svg")),
+);
 
-const fixtures = globSync(resolve(join(import.meta.dirname, "../../../sprites-generate/src/__tests__/fixture/svg/*.svg"))).map((im) => ({
+const fixtures = globSync(
+  resolve(join(import.meta.dirname, "../../../sprites-generate/src/__tests__/fixture/svg/*.svg")),
+).map((im) => ({
   svg: readFileSync(im),
   id: basename(im).replace(".svg", ""),
 }));
 
-console.log(fixtures);
-bench("generateLayout bench (concurrency=1,x20)", async () => {
+describe("scale 1x", () => {
+  bench("generate sprite @mapka/sprites-generate", async () => {
+    const layout = await mapkaGenerateLayout({
+      imgs: fixtures,
+      pixelRatio: 1,
+      format: false,
+    });
 
-});
+    await mapkaGenerateImage(layout);
+  });
 
-bench("generateLayout bench (concurrency=4,x20)", async () => {
-
+  bench("generate sprite @mapka/sprites-generate", async () => {
+    await new Promise((resolve) => {
+      mapboxGenerateLayout(
+        {
+          imgs: fixtures,
+          pixelRatio: 2,
+          format: false,
+        },
+        (err?: Error | null, layout?: ImgLayout | DataLayout) => {
+          if (err || !layout) {
+            throw err;
+          }
+          mapboxGenerateImage(layout as ImgLayout, (err?: Error | null, image?: Buffer) => {
+            if (err) {
+              throw err;
+            }
+            resolve(image);
+          });
+        },
+      );
+    });
+  });
 });
--- packages/sprites-generate/vitest.config.ts
+++ packages/sprites-generate/vitest.config.ts
@@ -1,7 +1,6 @@
 import { defineProject } from "vitest/config";
 
 export default defineProject({
-  
   test: {
     testTimeout: 10_000,  
     pool: "forks",
